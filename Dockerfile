9a1b8903e17ee506870f59c1d1c2fa75131bbf2c
Paulo Sampaio <paulo.ed@gmail.com>
2020-10-16 16:57:13 -0300

    adding vectorizer pipeline

--- Dockerfile
+++ Dockerfile
@@ -1,7 +1,11 @@
 FROM python:3.7-slim-buster
-RUN apt-get update && apt-get install -y build-essential
+RUN apt-get update && apt-get install -y \ 
+    build-essential \ 
+    ffmpeg \
+    libsm6 \
+    libxext6
 WORKDIR /usr/src/app
 COPY requirements.txt .
 RUN pip install --no-cache-dir -r requirements.txt
 COPY . .
-CMD ["python", "pipeline.py", "run"]
+CMD ["python", "image_vectorizer_pipeline.py", "run"]
--- image_vectorizer/image_vectorizing_functions.py
+++ image_vectorizer/image_vectorizing_functions.py
@@ -5,7 +5,7 @@ from tensorflow.keras.models import Model
 from tensorflow.keras.preprocessing.image import ImageDataGenerator
 
 
-def load_vectorizer_model():
+def _load_vectorizer_model():
     i = Input([224, 224, 3], dtype=np.float32)
     x = preprocess_input(i)
     core = Xception(include_top=False, pooling="max")
@@ -27,8 +27,9 @@ def _load_image_generator(file_path):
     return {"file_list": file_list, "image_generator": image_generator}
 
 
-def generate_vectors(file_path, model):
+def generate_vectors(file_path):
     file_list, image_generator = _load_image_generator(file_path).values()
+    model = _load_vectorizer_model()
     vectors = model.predict(
         image_generator, batch_size=128, verbose=1, use_multiprocessing=True
     )
