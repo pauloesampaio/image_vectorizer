6fecdae7461367b4c729c2c115ccf2675e65441d
pauloesampaio <paulo.ed@gmail.com>
2020-11-19 15:49:13 -0300

    Allow use of classes/labels

--- t-sne_plot.py
+++ t-sne_plot.py
@@ -3,16 +3,25 @@ from image_vectorizer.utils import load_config
 import pandas as pd
 from bokeh.plotting import figure, ColumnDataSource, show
 from bokeh.models import HoverTool
+from bokeh.palettes import Spectral6
+from bokeh.transform import factor_cmap
 
 
-config = load_config()
-xy = np.load(config["tsne_path"])
-paths_dataframe = pd.read_csv(config["file_list_path"])
+def build_plot(xy, paths, labels=None):
+    if labels:
+        colors = factor_cmap("labels", palette=Spectral6, factors=list(set(labels)))
+        plot_labels = labels
+    else:
+        colors = factor_cmap("labels", palette=Spectral6, factors=["None"])
+        plot_labels = ["None"] * len(xy)
 
-
-def build_plot(xy):
     source = ColumnDataSource(
-        data=dict(x=xy[:, 0], y=xy[:, 1], imgs=paths_dataframe["filename"].to_list())
+        data=dict(
+            x=xy[:, 0],
+            y=xy[:, 1],
+            imgs=paths,
+            labels=plot_labels,
+        ),
     )
     hover = HoverTool(
         tooltips="""
@@ -27,6 +36,7 @@ def build_plot(xy):
             <div>
                 <span style="font-size: 15px;">Location</span>
                 <span style="font-size: 10px; color: #696;">($x, $y)</span>
+                <span style="font-size: 10px;">Label: "@labels"</span>
             </div>
         </div>
         """
@@ -37,9 +47,31 @@ def build_plot(xy):
         plot_height=640,
         title="t-SNE plot (hover for images)",
     )
-    graph.scatter("x", "y", source=source, alpha=0.5, size=5)
+    if labels:
+        graph.scatter(
+            "x",
+            "y",
+            source=source,
+            alpha=0.5,
+            size=5,
+            color=colors,
+            legend_group="labels",
+        )
+    else:
+        graph.scatter("x", "y", source=source, alpha=0.5, size=5)
+
     return graph
 
 
-fig = build_plot(xy)
-show(fig)
+if __name__ == "__main__":
+    config = load_config()
+    xy = np.load(config["tsne_path"])
+    paths_dataframe = pd.read_csv(config["file_list_path"])
+    paths = paths_dataframe["filename"].tolist()
+    if config["infer_classes"]:
+        labels = paths_dataframe["class"].tolist()
+    else:
+        labels = None
+
+    fig = build_plot(xy, paths, labels)
+    show(fig)
