6dfae515f89870902a209aeddfa284250c50bfbd
pauloesampaio <paulo.ed@gmail.com>
2020-12-11 23:43:14 -0300

    adding docstrings

--- image_vectorizer/image_vectorizing_functions.py
+++ image_vectorizer/image_vectorizing_functions.py
@@ -6,6 +6,15 @@ from tensorflow.keras.preprocessing.image import ImageDataGenerator
 
 
 def load_vectorizer_model(reduce_dimensionality=False):
+    """Loads pre-trained Xception neural network to generate feature vectors.
+    If reduce_dimensionality, generates a 512-dim vector instead of 2048-dim.
+
+    Args:
+        reduce_dimensionality (bool, optional): If true, generates 512-dim vector. Defaults to False.
+
+    Returns:
+        tf.keras.model: Model to generate vectors
+    """
     i = Input([224, 224, 3], dtype=np.float32)
     x = preprocess_input(i)
     core = Xception(include_top=False, pooling="max")
@@ -19,6 +28,16 @@ def load_vectorizer_model(reduce_dimensionality=False):
 
 
 def load_image_generator(paths_dataframe, use_classes=False):
+    """Loads generator flowing through the image folder.
+    If use_classes, infer classes from parent folder.
+
+    Args:
+        paths_dataframe (str): Path to the images folder
+        use_classes (bool, optional): If true, infer classes using parent folder. Defaults to False.
+
+    Returns:
+        ImageDataGenerator: Image generator
+    """
     y_col, class_mode = None, None
     if use_classes:
         y_col = "class"
@@ -37,6 +56,18 @@ def load_image_generator(paths_dataframe, use_classes=False):
 
 
 def generate_vectors(paths_dataframe, use_classes=False, reduce_dimensionality=False):
+    """Function to generate vectors from the images on a given folder.
+    If use classes, infer class using parent folder name.
+    If reduce_dimensionality, generates a 512-dim vector (instead of a 2048-dim)
+
+    Args:
+        paths_dataframe (str): path to images folder
+        use_classes (bool, optional): If true, infer class using parent folder name. Defaults to False.
+        reduce_dimensionality (bool, optional): If true, generates a smaller, 512-dim vector. Defaults to False.
+
+    Returns:
+        np.array, list, list: Array with vectors, list with file pathes and, if use_classes, list of labels
+    """
     image_generator = load_image_generator(paths_dataframe, use_classes=use_classes)
     model = load_vectorizer_model(reduce_dimensionality)
     vectors = model.predict(
--- image_vectorizer/utils.py
+++ image_vectorizer/utils.py
@@ -7,12 +7,29 @@ import pandas as pd
 
 
 def load_config(config_path="./config/config.yml"):
+    """Simple helper to load configuration file
+
+    Args:
+        config_path (str, optional): Path to config.yml. Defaults to "./config/config.yml".
+
+    Returns:
+        [dict]: Configuration dictionary
+    """
     with open(config_path) as f:
         config = yaml.safe_load(f)
     return config
 
 
 def check_if_exists(path_to_check, create=False):
+    """Check if a path exists. Optionally, creates if needed.
+
+    Args:
+        path_to_check (string): Path to be checked
+        create (bool, optional): Create or not if needed. Defaults to False.
+
+    Returns:
+        None
+    """
     if not os.path.exists(path_to_check):
         if create:
             pathlib.Path(path_to_check).mkdir(parents=True, exist_ok=True)
@@ -24,10 +41,24 @@ def check_if_exists(path_to_check, create=False):
 
 
 def get_paths_dataframe(pictures_path, infer_classes=False):
-    path_list = glob.glob(
-        os.path.join(pictures_path, "**", "*.jpg"),
-        recursive=True,
-    )
+    """Get path of images (jpg, jpeg and png). In infer_classes, uses parent folder
+    as class name.
+
+    Args:
+        pictures_path (string): path to images folder
+        infer_classes (bool, optional): If true, use parent folder as class. Defaults to False.
+
+    Returns:
+        [pd.DataFrame]: Dataframe with file paths (and classes)
+    """
+    file_types = ["*.jpg", "*.jpeg", "*.png"]
+    path_list = []
+    for extension in file_types:
+        current_paths = glob.glob(
+            os.path.join(pictures_path, "**", extension),
+            recursive=True,
+        )
+        path_list.extend(current_paths)
     paths_dataframe = pd.DataFrame(path_list, columns=["filename"])
     if infer_classes:
         paths_dataframe["class"] = [
@@ -37,6 +68,15 @@ def get_paths_dataframe(pictures_path, infer_classes=False):
 
 
 def save_array(filepath, array):
+    """Save numpy array to file
+
+    Args:
+        filepath (str): Path to save array
+        array (np.array): Array to be saved
+
+    Returns:
+        None
+    """
     check_if_exists(os.path.dirname(filepath), create=True)
     with open(filepath, "wb") as f:
         np.save(f, array)
